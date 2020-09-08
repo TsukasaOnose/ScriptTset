a86fd300f386e13374842bdb612958d6220651ed
TsukasaOnose <70473575+TsukasaOnose@users.noreply.github.com>
2020-09-08 16:05:56 +0900

    Initial commit

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = { 0, 15, 255, 4095, 65535};
+
+        for (int i = 0; i < array.Length; i++)
+        {
+                Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
