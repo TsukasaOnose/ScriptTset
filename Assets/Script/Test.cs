4606bf1d5fd99243f96ed02a07846287398a83db
TsukasaOnose <70473575+TsukasaOnose@users.noreply.github.com>
2020-09-08 20:41:44 +0900

    発展課題
    Magic関数を呼び出し、mpを消費してMPが足りなくなるまで魔法を使う。

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    public int mp = 53;
+
+    public void Magic(int mpCost)
+    {
+        if (this.mp >= 5)
+        {
+            this.mp -= mpCost;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.mp + "。");
+        }
+        else
+        {
+            this.mp -= mpCost;
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        int[] array = { 0, 15, 255, 4095, 65535};
+        Boss zakoboss = new Boss();
 
-        for (int i = array.Length - 1; i >=0; i--)
-        {
-                Debug.Log(array[i]);
-        }
+        for (zakoboss.mp = 53; zakoboss.mp > 0; zakoboss.Magic(5));
     }
 
     // Update is called once per frame
