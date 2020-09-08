fe4792acbdc5bce84915a78b8ec522e9bbb4ad89
TsukasaOnose <70473575+TsukasaOnose@users.noreply.github.com>
2020-09-08 17:15:26 +0900

    配列の表示の逆順バージョンその2
    要素の個数が分からない場合でも対応できるようにコードを書き換えた

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -9,7 +9,7 @@ public class Test : MonoBehaviour
     {
         int[] array = { 0, 15, 255, 4095, 65535};
 
-        for (int i = 4; i >=0 && i <= array.Length; i--)
+        for (int i = array.Length - 1; i >=0; i--)
         {
                 Debug.Log(array[i]);
         }
