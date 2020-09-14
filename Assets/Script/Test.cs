8e52c47f71bf2b3a16c177ed59ddef55284c7079
TsukasaOnose <70473575+TsukasaOnose@users.noreply.github.com>
2020-09-14 17:05:43 +0900

    Lesson4の課題再提出
    基本課題と発展課題を両方確認できるようにしたもの

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -26,7 +26,18 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Boss zakoboss = new Boss();
+        int[] array = { 0, 1, 2, 3, 4 };
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+            Boss zakoboss = new Boss();
 
         for (zakoboss.mp = 53; zakoboss.mp > 0; zakoboss.Magic(5));
     }
